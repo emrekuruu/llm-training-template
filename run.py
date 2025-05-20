ce074bc98bc9db1233b7e81c80298daa3ed79870
emrekuruu <emre.kuru@ozu.edu.tr>
2025-05-20 21:42:16 +0300

    feat: train and evaluate mode

--- run.py
+++ run.py
@@ -1,16 +1,21 @@
 import hydra
 from omegaconf import DictConfig
 from src.utils import init_wandb
+from src import trainer, eval
 
 @hydra.main(config_path="configs", config_name="config", version_base="1.3")
 def main(cfg: DictConfig):
     if cfg.logging.enable_wandb:
         init_wandb(cfg)
 
-    print("Hydra config loaded successfully.")
-    print(cfg)
-
-    # TODO: Add call to training loop here (e.g., train(cfg))
+    if cfg.mode == "train":
+        ## TODO: Add training loop here
+        print("Training...")
+    elif cfg.mode == "eval":
+        ## TODO: Add evaluation loop here
+        print("Evaluating...")
+    else:
+        raise ValueError(f"Unsupported mode: {cfg.mode}")
 
 if __name__ == "__main__":
     main()
